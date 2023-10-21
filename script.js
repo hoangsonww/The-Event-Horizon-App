8f4bbdafa1afe1296b3b24ed9e1d638845ba6336
Son Nguyen Hoang <124531104+hoangsonww@users.noreply.github.com>
2023-10-21 04:43:04 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ document.addEventListener('DOMContentLoaded', function() {
     if (lastEvent) {
         eventTitle.textContent = lastEvent.name;
         currentEventDate = lastEvent.date;
-    } else { // This part is the fallback to default event
+    } else {
         eventTitle.textContent = "New Year 2024";
         currentEventDate = "1 Jan 2024";
     }
@@ -151,7 +151,6 @@ function displayCustomEvents() {
     customEvents.forEach((event, index) => {
         const li = document.createElement('li');
 
-        // Create the event text span
         const eventSpan = document.createElement('span');
         eventSpan.textContent = `${event.name} (${event.date})`;
         eventSpan.addEventListener('click', function() {
@@ -160,7 +159,6 @@ function displayCustomEvents() {
             countdown();
         });
 
-        // Create the delete button
         const deleteBtn = document.createElement('button');
         deleteBtn.textContent = 'Delete';
         deleteBtn.addEventListener('click', function(e) {
@@ -201,10 +199,80 @@ function formatTime(time) {
     return time < 10 ? `0${time}` : time;
 }
 
+function elizaResponse(message) {
+    const lowerMessage = message.toLowerCase();
+    if (lowerMessage.includes("hello") || lowerMessage.includes("hi")) {
+        return "Hello! How can I help you with your event countdowns today?";
+    } else if (lowerMessage.includes("about")) {
+        return "EventHorizon is an app to help you count down to significant events in your life. You can select from predefined events or set up your custom events.";
+    } else if (lowerMessage.includes("how are you")) {
+        return "I'm just a bot, but I'm here and ready to help!";
+    } else if (lowerMessage.includes("event")) {
+        return "You can create countdowns for different events. If you have a custom event, just specify its name and date!";
+    } else if (lowerMessage.includes("background")) {
+        setRandomBackgroundImage();
+        displayRandomQuote();
+        return "Background and quote updated!";
+    } else if (lowerMessage.includes("quote")) {
+        displayRandomQuote();
+        return "Quote refreshed!";
+    } else if (lowerMessage.includes("thanks") || lowerMessage.includes("thank you")) {
+        return "You're welcome! Let me know if you have more questions.";
+    } else if (lowerMessage.includes("who are you")) {
+        return "I'm the EventHorizon Assistant. I'm here to guide you on using this app and answer any questions you might have.";
+    } else if (lowerMessage.includes("features")) {
+        return "You can set countdowns for different events, choose from predefined ones or create your custom event, change the background, get inspired by a quote, and now you can chat with me for more information!";
+    } else if (lowerMessage.includes("joke")) {
+        return "Why did the calendar factory get shut down? Because it took a day off!";
+    } else if (lowerMessage.includes("countdown")) {
+        return "EventHorizon helps you create countdowns for special events in your life, keeping track of the days, hours, minutes, and seconds until your event!";
+    } else if (lowerMessage.includes("custom event")) {
+        return "You can create a custom event by entering its name and date. Just use the 'Set Event' button!";
+    } else if (lowerMessage.includes("inspiration")) {
+        return "Get inspired! Ask for an inspirational quote, and I'll provide you with some motivational words.";
+    } else if (lowerMessage.includes("help")) {
+        return "I'm here to assist! Feel free to ask about events, backgrounds, quotes, or any questions you have about EventHorizon.";
+    } else if (lowerMessage.includes("bye") || lowerMessage.includes("goodbye")) {
+        return "Farewell! Don't hesitate to return if you have more questions. Happy event-planning!";
+    } else {
+        return "I'm not sure about that, can you ask differently or specify more?";
+    }
+}
+
+const chatbotInput = document.getElementById("chatbotInput");
+const chatbotBody = document.getElementById("chatbotBody");
+
+chatbotInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        sendMessage(chatbotInput.value);
+        chatbotInput.value = "";
+    }
+});
+
+function sendMessage(message) {
+    chatbotBody.innerHTML += `
+        <div style="text-align: right; margin-bottom: 10px; color: white;">${message}</div>
+    `;
+    let botReply = elizaResponse(message);
+    setTimeout(() => {
+        chatbotBody.innerHTML += `
+            <div style="text-align: left; margin-bottom: 10px; color: white;">${botReply}</div>
+        `;
+    }, 1000);
+}
+
+const changeBackgroundBtn = document.createElement("button");
+changeBackgroundBtn.textContent = "Change Background";
+changeBackgroundBtn.style.position = "fixed";
+changeBackgroundBtn.style.bottom = "50px";
+changeBackgroundBtn.style.left = "10px";
+changeBackgroundBtn.onclick = function() {
+    setRandomBackgroundImage();
+    displayRandomQuote();
+};
+document.body.appendChild(changeBackgroundBtn);
+
 countdown();
-
 displayRandomQuote();
-
 setRandomBackgroundImage();
-
 setInterval(countdown, 1000);
